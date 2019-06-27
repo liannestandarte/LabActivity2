49dff21144718e4d2ac497326be6608e18f347ee
cisco2018 <2015081633@ust-ics.mygbiz.com>
2019-06-27 23:31:42 +0800

    Inital commit

--- app/src/main/java/com/example/labactivity2/MainActivity.java
+++ app/src/main/java/com/example/labactivity2/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.labactivity2;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
